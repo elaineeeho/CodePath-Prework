fe4e947512aeeeb9bbe96a7faac18255480c0328
Elane Ho <elaine_ho@ucsb.edu>
2021-12-16 21:30:52 -0800

    Initial Commit

---
+++ pre-work/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pre-work
+//
+//  Created by elaine ho on 12/16/21.
+//  Copyright © 2021 elaine ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
